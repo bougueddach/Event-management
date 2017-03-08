741cedc8042fd9f7d13ed44c629d94ccc2f9de8f
Sarah LOTFI <Sarah LOTFI@192.168.1.5>
2017-03-08 20:36:47 +0000

    hello sarah

--- src/event/management/EventManagement.java
+++ src/event/management/EventManagement.java
@@ -17,6 +17,7 @@ public class EventManagement {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.printf("hello");
+        System.out.printf("hello sarah");
     }
     
 }
