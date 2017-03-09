0481786f3fb37646ddb78c639f335f40c44a8d2c
SarahL21 <sarahlotfi10@gmail.com>
2017-03-09 20:52:19 +0000

    BD finale

--- events.sql
+++ events.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 09 Mars 2017 à 00:31
+-- Généré le :  Jeu 09 Mars 2017 à 21:51
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -29,8 +29,9 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `departement` (
   `IDdept` int(11) NOT NULL AUTO_INCREMENT,
   `NomDept` varchar(10) NOT NULL,
+  `IDChef` int(11) NOT NULL,
   PRIMARY KEY (`IDdept`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -56,6 +57,7 @@ CREATE TABLE IF NOT EXISTS `employe` (
 --
 
 CREATE TABLE IF NOT EXISTS `event` (
+  `IDEvent` int(11) NOT NULL AUTO_INCREMENT,
   `Titre` varchar(40) NOT NULL,
   `Secteur` varchar(20) NOT NULL,
   `Thème` varchar(40) NOT NULL,
@@ -64,7 +66,32 @@ CREATE TABLE IF NOT EXISTS `event` (
   `Description` varchar(20) NOT NULL,
   `Type` varchar(20) NOT NULL,
   `IDorg` int(10) NOT NULL,
+  PRIMARY KEY (`IDEvent`),
   UNIQUE KEY `IDorg` (`IDorg`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `eventdept`
+--
+
+CREATE TABLE IF NOT EXISTS `eventdept` (
+  `IDEvent` int(11) NOT NULL,
+  `IDDept` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participations`
+--
+
+CREATE TABLE IF NOT EXISTS `participations` (
+  `IDEvent` int(11) NOT NULL,
+  `IDEmp` int(11) NOT NULL,
+  `Date` int(11) NOT NULL,
+  PRIMARY KEY (`IDEvent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
